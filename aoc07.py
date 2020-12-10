a664bb31be489382a98ee11841b5263bd698d6f2
Herman van der Veer <hvdveer@gmail.com>
2020-12-10 14:30:31 +0100

    add aoc7a

--- aoc07.py
+++ aoc07.py
@@ -2,46 +2,26 @@
 import re
 
 content = [line.rstrip('\n') for line in open("input/aoc07_input.txt")]
-rules = [re.split('[\s]', line) for line in content]
-
-class Bag:
-    def __init__(self, bag, nr1=None, sub_bag1=None, nr2=None, sub_bag2=None):
-        self.bag = bag
-        self.nr1 = nr1
-        self.sub_bag1 = sub_bag1
-        self.nr2 = nr2
-        self.sub_bag2 = sub_bag2
-
-    def __str__(self):
-        return f"{self.bag} is empty"
-
-    def __str__(self):
-        return f"{self.bag} bags contain {str(self.nr1)} {self.sub_bag1} bags."
-
-    # def __str__(self, bag, nr1, sub_bag1, nr2, sub_bag2):
-    #     return f"{self.bag} bags contain {self.nr1} {self.sub_bag1} bags, {self.nr2} {self.sub_bag2} bags."
+bags = [re.findall(r'\w+\s\w+(?=\s+bag)', line) for line in content]
 
 
-def sentence_to_bag(sentence):
-    super_bag = sentence[0:2]
-    if sentence[4] == "no":
-        return super_bag
-    nr1, sub_bag1 = int(sentence[4]), sentence[5:7]
-    if len(sentence) < 9:
-        return super_bag, nr1, sub_bag1
-    else:
-        nr2, sub_bag2 = int(sentence[8]), sentence[9:11]
-        return super_bag, nr1, sub_bag1, nr2, sub_bag2
-
-test = Bag("brown", 2, "yellow")
-print(test)
+def check_bag(color):
+    contain_color = set()
+    for bag in bags:
+        if color in bag[1:]:
+            contain_color.add(bag[0])
+    return contain_color
 
 
-# bag_list = []
-# for sentence in rules:
-#     bag_list.append(Bag(sentence_to_bag(sentence)))
-#
-# print(bag_list)
-#
-# for bag in bag_list:
-#     print(bag)
+def find_bags(color):
+    for sub_bag in check_bag(color):
+        found_bags.add(sub_bag)
+        if sub_bag in check_bag(color):
+            find_bags(sub_bag)  # recursion
+
+
+# How many bag colors can eventually contain at least one shiny gold bag?
+found_bags = set()
+find_bags("shiny gold")
+print(found_bags)
+print(len(found_bags))
